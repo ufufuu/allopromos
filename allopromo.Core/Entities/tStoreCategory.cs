8dcd7a9810ba4a91bdc7b82c15ac0f56ae324d62
Bik Wiy <info@enov.tech>
2022-10-31 17:58:16 -0400

    allopromo

--- allopromo.Core/Entities/tStoreCategory.cs
+++ allopromo.Core/Entities/tStoreCategory.cs
@@ -9,11 +9,11 @@ namespace allopromo.Core.Entities
     {
         [Key]
         //[DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
-        //public Guid StoreCategoryId { get; set; }
-
+        
         [ForeignKey("Category")]
         public Guid storeCategoryId { get; set; }
         public string storeCategoryName { get; set; }
+
         //public bool hasChildren { get; set; }
         //public string storeCategoryImageUrl { get; set; }
         //public ICollection<tStore> tStores { get; set; } = new List<tStore>();
