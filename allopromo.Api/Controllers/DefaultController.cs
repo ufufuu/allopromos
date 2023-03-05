e8b22c38077796b5d1d10e35b5534d253f5776ab
Bik Wiy <info@enov.tech>
2023-03-05 03:46:50 -0500

    allopromo

--- allopromo.Api/Controllers/DefaultController.cs
+++ allopromo.Api/Controllers/DefaultController.cs
@@ -23,7 +23,7 @@ namespace allopromo.Controllers
         {
             try
             {
-                var defaultResult = "hello Default Controller this March, 05, 2023 - 3h 26 Am";
+                var defaultResult = "hello Default Controller this March, 05, 2023 - 3h 46 Am";
                 return Ok(defaultResult);
             }
             catch(Exception ex)
