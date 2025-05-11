45209390cb711bdc8e9fb7377f966858ebdab8c4
Bik Wiy <info@enov.tech>
2025-05-11 14:03:18 +0000

    added Validator

---
+++ src/allopromo.Api/Validators/ProductCategoryValidator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace allopromo.Api.Validators
+{
+    public class ProductCategoryValidator : IValidator
+    {
+        bool IValidator.CanValidateInstancesOfType(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        IValidatorDescriptor IValidator.CreateDescriptor()
+        {
+            throw new NotImplementedException();
+        }
+
+        ValidationResult IValidator.Validate(IValidationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ValidationResult> IValidator.ValidateAsync(IValidationContext context, CancellationToken cancellation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ src/allopromo.Api/Validators/StoreCategoryValidator.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using allopromo.Core.Entities;
+using FluentValidation;
+using FluentValidation.Results;
+
+namespace allopromo.Api.Validators
+{
+    public class StoreCategoryValidator : IValidator<StoreCategory>
+    {
+        bool IValidator.CanValidateInstancesOfType(Type type)
+        {
+            throw new NotImplementedException();
+        }
+
+        IValidatorDescriptor IValidator.CreateDescriptor()
+        {
+            throw new NotImplementedException();
+        }
+
+        ValidationResult IValidator<StoreCategory>.Validate(StoreCategory instance)
+        {
+            throw new NotImplementedException();
+        }
+
+        ValidationResult IValidator.Validate(IValidationContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ValidationResult> IValidator<StoreCategory>.ValidateAsync(StoreCategory instance, CancellationToken cancellation)
+        {
+            throw new NotImplementedException();
+        }
+
+        Task<ValidationResult> IValidator.ValidateAsync(IValidationContext context, CancellationToken cancellation)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
